363dff93e1ac056e23ce22dee584a30ea2f7abf5
Nathan Chen <113192742+seribical@users.noreply.github.com>
2024-04-19 13:11:51 -0400

    help

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <form>
-<label for="fname">First name:</label><br>
-<input type="text" id="fname" name="fname"><br>
-<label for="lname">Last name:</label><br>
-<input type="text" id="lname" name="lname"><br>
-<button>sighin</button>
+<label for="fname">Email:</label><br>
+<input type="text" id="email" name="fname"><br>
+<label for="lname">Password</label><br>
+<input type="text" id="pass" name="lname"><br>
+
 </form>
+<button @click="signUpNewUser()">sighin</button>
+
+
 </template>
 
 <script setup>
@@ -14,10 +17,10 @@ import { supabase } from './lib/supabaseClient'
 
 async function signUpNewUser() {
   const { data, error } = await supabase.auth.signUp({
-    email: 'example@email.com',
-    password: 'example-password',
+    email: document.querySelector('#email').value,
+    password: document.querySelector('#pass').value,
     options: {
-      emailRedirectTo: 'https://example.com/welcome',
+      emailRedirectTo: 'www.google.com',
     },
   })
 }
@@ -27,5 +30,10 @@ async function signInWithEmail() {
     email: 'example@email.com',
     password: 'example-password',
   })
+}
+
+function gyat(){
+  console.log(document.querySelector('#fname').value)
+  
 }
 </script>
