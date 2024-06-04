c86efa41f04756c5b1c8e10c3460479564d64d15
Nathan Chen <113192742+seribical@users.noreply.github.com>
2024-06-03 20:33:46 -0400

    Update CharacterIndex.vue

--- supabase-vue-3/src/views/CharacterIndex.vue
+++ supabase-vue-3/src/views/CharacterIndex.vue
@@ -1,22 +1,27 @@
 <script setup>
 import { supabase } from '../supabase';
 import {onMounted} from 'vue'
+import { ref } from 'vue';
 
-
-let data = []
+let characters = ref([])
+let people = []
 async function call(){
     const {data} = await supabase
     .from('characters')
     .select()
 
-    data = data
+    characters = data
 }
+
+
 
 onMounted(()=>{
     call()
 })
 
-console.log(data[0])
+console.log(characters)
+
+
 </script>
 
 <template>
